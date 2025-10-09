05f01bf126e19b2cf449cdb569af8767707591b6
Elizabeth <132071363+lizzi21@users.noreply.github.com>
2025-10-09 03:29:51 +0300

    add all pages

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -11,9 +11,14 @@ function createPages(template, filename) {
 const htmlPages = [
     createPages('./src/index.html', './index.html'),
     createPages('./src/pages/articles.html', './pages/articles.html'),
-    createPages('./src/pages/dictionary.html', './pages/dictionary.html'),
+    createPages('./src/pages/guides.html', './pages/guides.html'),
     createPages('./src/pages/tests.html', './pages/tests.html'),
     createPages('./src/pages/articles/article1.html', './pages/articles/article1.html'),
+    createPages('./src/pages/articles/article2.html', './pages/articles/article2.html'),
+    createPages('./src/pages/articles/article3.html', './pages/articles/article3.html'),
+    createPages('./src/pages/guides/guid1.html', './pages/guides/guid1.html'),
+    createPages('./src/pages/guides/guid2.html', './pages/guides/guid2.html'),
+    createPages('./src/pages/guides/guid3.html', './pages/guides/guid3.html'),
     createPages('./src/pages/tests/test1.html', './pages/tests/test1.html')
 ]
 
